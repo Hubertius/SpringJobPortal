8b66859afd456c9fb449c90a08394c0a9b6a8b4e
Hubertius <hubert.kopec@interia.pl>
2025-01-07 01:09:33 +0100

    Add of user entities
    Signed-off-by: Hubertius <hubert.kopec@interia.pl>

---
+++ src/main/java/com/web/jobportal/entity/UserType.java
@@ -0,0 +1,52 @@
+package com.web.jobportal.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name="users_type")
+public class UserType {
+
+    private int userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(targetEntity = User.class, mappedBy="userTypeId", cascade = CascadeType.ALL)
+    private List<User> users;
+
+    public int getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(int userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UserType{" +
+                "userTypeId=" + userTypeId +
+                ", userTypeName='" + userTypeName + '\'' +
+                '}';
+    }
+}
